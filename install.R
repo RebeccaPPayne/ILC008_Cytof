271212cb34b2db5ac3f7f76d582350ec6351f2d1
RebeccaPPayne <rebecca.payne2@ncl.ac.uk>
2021-04-26 22:40:23 +0100

    delete description and create install.R

---
+++ install.R
@@ -0,0 +1,14 @@
+install.packages(c("CATALYST",
+                   "cowplot",
+                   "flowCore",
+                   "ggplot2",
+                   "knitr",
+                   "markdown",
+                   "openxlsx",
+                   "readxl",
+                   "rmarkdown",
+                   "scater",
+                   "scatterplot3d",
+                   "SingleCellExperiment")
+                   
+          
